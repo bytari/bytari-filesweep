e006516b6de91c7f42b9f0eaae8ff34214ed61b2
Bytari <bytari.dev@gmail.com>
2026-08-26 13:11:56 -0400

    Add documentation for sorter.py module
    Added module documentation outlining responsibilities.

--- filesweep/modules/sorter.py
+++ filesweep/modules/sorter.py
@@ -1,1 +1,7 @@
+# sorter.py
+# Sorts files into gategorized folders based on extension.
 
+# Responsibilities:
+# - identify file type
+# - Create folders if missing
+# - Move files to correct folder
